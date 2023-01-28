c29c09fca81219252ea621cba6164f71136bd3ee
ppauline17 <p.pauline017@gmail.com>
2023-01-28 19:37:39 +0100

    version 1

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Pauline</title>
+    <meta name="description" content="CV Pauline PASQUIER">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/font.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+        <div class="en-tete">
+            <h1>Pauline Pasquier</h1>
+            <h2>Développeuse Web en formation</h2>
+            <img class="photo" src="img/IMG_2827.jpg" alt="Ma photo" height="300px"/>
+        </div>
+        
+        
+        <nav>
+            <ul>
+                <li><a href="#parcours" title="Parcours">Parcours</a></li>
+                <li><a href="#realisations" title="Mes réalisations">Réalisations</a></li>
+                <li><a href="#contact" title="Contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <img src="img/computer.jpg" alt="image d'ordinateur">
+        <div class="container">
+            <h1 id="parcours">Parcours</h1>
+            <h2>Présentation</h2>
+            <p>Après 13 ans en tant qu'opticienne diplômée, j'ai décidé de me reconvertir dans le web. Je suis actuellement en formation à L'ISFAC de Poitiers pour obtenir un Titre Professionnel de Développeur Web/Web mobile.</p>
+            <!-- <ul>
+                <li>De 2009 à 2011 EURO OPTIQUE (Confolens, 16) Apprentie opticienne</li>
+                <li>De 2011 à 2012 EURO OPTIQUE (Confolens, 16) Opticienne collaboratrice</li>
+                <li>De 2012 à 2016 OPTICAL CENTER (Chasseneuil du poitou, 86) Opticienne collaboratrice</li>
+                <li>De 2016 à 2019 OPTIC & PRICE (Migné-Auxances 86) Opticienne responsable</li>
+                <li>De 2019 à 2020 ECOUTER VOIR (Poitiers, 86) Opticienne collaboratrice</li>
+                <li>De Mars 2020 à Août 2022 OPTIC 2000 (Poitiers, 86) Opticienne collaboratrice</li>
+            </ul> -->
+            <h1 id="realisations">Réalisations</h1>
+            <h1 id="contact">Contact</h1>
+            <form action="traitement.php" method="post">
+                <div>Nom : <input type="text" name="nom"></div>
+                <div>Prénom : <input type="text" name="prenom"></div>
+                <div>Email : <input type="email" name="email"></div>
+                <div>Sujet : <input type="text" name="sujet"></div>
+                <div>Message : <textarea name="message"></textarea></div>
+                <div>En validant ce formulaire, vous acceptez d'être contacté <input type="checkbox" name="accepte"></div>
+                <input type="submit" id="submit"value="Envoyer">
+        
+            </form>
+        </div>
+        
+    </main>
+
+    <script>
+        
+    </script>
+</body>
+</html>
