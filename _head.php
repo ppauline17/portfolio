11958e69e0cb2bbd52c6b63d04ff9cdafb15d5c7
ppauline17 <p.pauline017@gmail.com>
2023-11-04 00:35:52 +0100

    tag

--- _head.php
+++ _head.php
@@ -10,3 +10,12 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LVPBGZKB7Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LVPBGZKB7Y');
+</script>
